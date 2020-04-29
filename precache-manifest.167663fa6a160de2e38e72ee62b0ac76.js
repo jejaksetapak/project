269801ce376f0d52320f4d510ade81c09e19e8ef
Riswanto <37138545+wansr87@users.noreply.github.com>
2020-04-29 11:01:16 +0700

    Updates

---
+++ precache-manifest.167663fa6a160de2e38e72ee62b0ac76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e02ffbec890fc017b4480bbe0ed2dc7",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "24b45bec8c5e65abd511",
+    "url": "/project/static/css/main.340a57ac.chunk.css"
+  },
+  {
+    "revision": "4baf40d9f6e86320b7a9",
+    "url": "/project/static/js/2.b2a5ee49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project/static/js/2.b2a5ee49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b45bec8c5e65abd511",
+    "url": "/project/static/js/main.9d1566ef.chunk.js"
+  },
+  {
+    "revision": "cea028d6efb43d0c9732",
+    "url": "/project/static/js/runtime-main.d2f9efed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jejaksetapak/project.git/precache-manifest.8567a46929c1ad6735d90d1f2c07728f.js"
+  "/project/precache-manifest.167663fa6a160de2e38e72ee62b0ac76.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jejaksetapak/project.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
